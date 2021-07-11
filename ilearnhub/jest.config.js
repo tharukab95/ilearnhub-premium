860236e34e157aab9e5de74b1250704ed29ac153
tharukab95 <acslashed@yahoo.com>
2021-07-11 16:25:55 +0530

    created tree map for admin app and a login lib

--- ilearnhub/jest.config.js
+++ ilearnhub/jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  projects: ['<rootDir>/apps/tutor-portal', '<rootDir>/apps/tutee-portal'],
+  projects: [
+    '<rootDir>/apps/tutor-portal',
+    '<rootDir>/apps/tutee-portal',
+    '<rootDir>/apps/admin-portal',
+    '<rootDir>/libs/auth',
+  ],
 };
