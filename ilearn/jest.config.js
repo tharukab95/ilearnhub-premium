0a425fb399e1e028bccc7ab879a7d9e175b0abb5
tharukab95 <acslashed@yahoo.com>
2021-07-13 03:31:28 +0530

    removed again

--- ilearn/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  projects: [
-    '<rootDir>/apps/tuition-portal',
-    '<rootDir>/apps/api',
-    '<rootDir>/apps/admin-portal',
-    '<rootDir>/apps/payment-portal',
-    '<rootDir>/libs/shared/auth',
-  ],
-};
