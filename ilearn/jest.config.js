27237170b4dc29d23a5fb4c1c9395ff2e21651fc
tharukab95 <acslashed@yahoo.com>
2021-07-13 03:44:28 +0530

    generated adming-portal angular app

--- ilearn/jest.config.js
+++ ilearn/jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/tuition-portal', '<rootDir>/apps/payment-portal'],
+  projects: [
+    '<rootDir>/apps/tuition-portal',
+    '<rootDir>/apps/payment-portal',
+    '<rootDir>/apps/admin-portal',
+  ],
 };
