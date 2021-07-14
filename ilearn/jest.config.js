a50aa8b2ecd7d6a0a9af973b537ab6dc2a2c07b8
tharukab95 <acslashed@yahoo.com>
2021-07-14 18:43:58 +0530

    deleted previous workspace

--- ilearn/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  projects: [
-    '<rootDir>/apps/tuition-portal',
-    '<rootDir>/apps/payment-portal',
-    '<rootDir>/apps/admin-portal',
-    '<rootDir>/libs/auth',
-    '<rootDir>/libs/data-models',
-  ],
-};
